a60998173ce215bdaeb3a33db334baf280e12337
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-09-05 19:08:51 +0300

    pushed but not complete

--- store/cartStore.js
+++ store/cartStore.js
@@ -2,6 +2,20 @@ import { decorate, observable } from "mobx";
 
 class CartStore {
   items = [];
+
+  addItemToCart = addItem => {
+    console.log("HELLO HUSS", addItem);
+
+    this.items.push(addItem);
+  };
+
+  removeItemFromCart = removeItem => {
+    this.items = this.items.filter(cartItem => cartItem !== item);
+  };
+
+  checkoutCart = checkOut => {
+    this.items = [];
+  };
 }
 
 decorate(CartStore, {
